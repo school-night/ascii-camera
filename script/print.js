5734f4b3a41a9bad9bec7cf4a139d944c80dd7c7
Zeke Wattles <zekewattles@gmail.com>
2019-04-08 10:07:22 +0200

    changes

--- script/print.js
+++ script/print.js
@@ -1,14 +1,20 @@
 (function() {
     let file
-    const asciiEl = document.getElementById('ascii')
+    
     const aEl = document.createElement('a')
     const aElText = document.createTextNode("Download ascii"); 
     aEl.appendChild(aElText)
 })();
 
-var capture = (function() {
-    file = new Blob([asciiEl.innerHTML], {type: 'text/plain'})
-    aEl.href = URL.createObjectURL(file)
-    aEl.download = 'myfilename.txt'
-    document.querySelectorAll('body')[0].appendChild(aEl)
-})();
+const getAscii = function() {
+    const asciiEl = document.getElementById('ascii')
+    return asciiEl.innerHTML
+}
+
+var download = function() {
+    console.log('dload')
+    // file = new Blob([asciiEl.innerHTML], {type: 'text/plain'})
+    // aEl.href = URL.createObjectURL(file)
+    // aEl.download = 'myfilename.txt'
+    // document.querySelectorAll('body')[0].appendChild(aEl)
+}
