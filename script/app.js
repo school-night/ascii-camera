d2bdfb5a055c475b250af564664a610ab4a99fd0
ZEKEWATTLES <ZEKEWATTLES@GMAIL.COM>
2019-07-11 17:44:22 -0700

    styling

--- script/app.js
+++ script/app.js
@@ -6,63 +6,75 @@
  * Released under the MIT license
  */
 
-function populateDownloadLink ({ downloadEl, asciiContainer }) {
-	const ascii = asciiContainer.innerHTML
-	const modifiedAscii = ascii // kill this line
-	// const modifiedAscii = ascii + "\nur in zeke's world now" // custom message (\n is new line)
-	const file = new Blob([modifiedAscii], {type: 'text/plain'}) // frame data
-	downloadEl.href = URL.createObjectURL(file) // inject frame into download link
-	downloadEl.download = 'ascii.txt' // set file name
+// get the current date
+var curday = function(sp) {
+  today = new Date();
+  var dd = today.getDate();
+  var mm = today.getMonth() + 1; //As January is 0.
+  var yyyy = today.getFullYear();
+
+  if (dd < 10) dd = "0" + dd;
+  if (mm < 10) mm = "0" + mm;
+  return mm + sp + dd + sp + yyyy;
+};
+// console.log(curday('.'));
+
+function populateDownloadLink({ downloadEl, asciiContainer }) {
+  const ascii = asciiContainer.innerHTML;
+  // const modifiedAscii = ascii // kill this line
+  const modifiedAscii = ascii + "\ntaken on " + curday(".") + " by zeke.studio"; // custom message (\n is new line)
+  const file = new Blob([modifiedAscii], { type: "text/plain" }); // frame data
+  downloadEl.href = URL.createObjectURL(file); // inject frame into download link
+  downloadEl.download = "ascii.txt"; // set file name
 }
 
 (function() {
-	const asciiContainer = document.getElementById("ascii");
-	const downloadEl = document.getElementById('download')
-	let capturing = false;
+  const asciiContainer = document.getElementById("ascii");
+  const downloadEl = document.getElementById("download");
+  let capturing = false;
 
-	camera.init({
-		width: 84,
-		height: 62,
-		fps: 30,
-		mirror: true,
+  camera.init({
+    width: 84,
+    height: 62,
+    fps: 30,
+    mirror: true,
 
-		onFrame: function(canvas) {
-			ascii.fromCanvas(canvas, {
-				// contrast: 128,
-				callback: function(asciiString) {
-					asciiContainer.innerHTML = asciiString;
-				}
-			});
-			populateDownloadLink({ downloadEl, asciiContainer })
-		},
+    onFrame: function(canvas) {
+      ascii.fromCanvas(canvas, {
+        // contrast: 128,
+        callback: function(asciiString) {
+          asciiContainer.innerHTML = asciiString;
+        }
+      });
+      populateDownloadLink({ downloadEl, asciiContainer });
+    },
 
-		onSuccess: function() {
-			document.getElementById("info").style.display = "none";
+    onSuccess: function() {
+      document.getElementById("info").style.display = "none";
 
-			const button = document.getElementById("button");
-			button.style.display = "block";
-			button.onclick = function() {
-				if (capturing) {
-					camera.pause();
-					downloadEl.click()
-					button.innerText = 'start';
-				} else {
-					camera.start();
-					button.innerText = 'capture';
-				}
-				capturing = !capturing;
-			};
-		},
+      const button = document.getElementById("button");
+      button.style.display = "block";
+      button.onclick = function() {
+        if (capturing) {
+          camera.pause();
+          downloadEl.click();
+          button.innerText = "Start the camera";
+        } else {
+          camera.start();
+          button.innerText = "Take a picture";
+        }
+        capturing = !capturing;
+      };
+    },
 
-		onError: function(error) {
-			// TODO: log error
-		},
+    onError: function(error) {
+      // TODO: log error
+    },
 
-		onNotSupported: function() {
-			document.getElementById("info").style.display = "none";
-			asciiContainer.style.display = "none";
-			document.getElementById("notSupported").style.display = "block";
-		}
-	});
+    onNotSupported: function() {
+      document.getElementById("info").style.display = "none";
+      asciiContainer.style.display = "none";
+      document.getElementById("notSupported").style.display = "block";
+    }
+  });
 })();
-
